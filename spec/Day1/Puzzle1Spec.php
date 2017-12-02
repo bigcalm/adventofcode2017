847285e58078b51e77cdad322f0cc58058ee3371
Iain Cuthbertson <bigcuthy@gmail.com>
2017-12-02 17:41:57 +0000

    Day 1, puzzle 1

--- spec/Day1/Puzzle1Spec.php
+++ spec/Day1/Puzzle1Spec.php
@@ -11,4 +11,58 @@ class Puzzle1Spec extends ObjectBehavior
     {
         $this->shouldHaveType(Puzzle1::class);
     }
+
+    public function it_converts_input_sting_into_an_array()
+    {
+        $data = $this->parseString('1122');
+
+        $data->shouldBeArray();
+        $data->shouldHaveCount(4);
+    }
+
+    public function it_gets_next_in_sequence()
+    {
+        $data = $this->parseString('1122');
+        $this->getNextInSequence($data, 0)->shouldBe(1);
+        $this->getNextInSequence($data, 1)->shouldBe(2);
+        $this->getNextInSequence($data, 2)->shouldBe(2);
+        $this->getNextInSequence($data, 3)->shouldBe(1);
+
+        $data = $this->parseString('12345');
+        $this->getNextInSequence($data, 0)->shouldBe(2);
+        $this->getNextInSequence($data, 1)->shouldBe(3);
+        $this->getNextInSequence($data, 2)->shouldBe(4);
+        $this->getNextInSequence($data, 3)->shouldBe(5);
+        $this->getNextInSequence($data, 4)->shouldBe(1);
+    }
+
+    public function it_calculates_captcha_to_be_3_for_input_1122()
+    {
+        $data = $this->parseString('1122');
+
+        $captcha = $this->calculateCaptcha($data);
+        $captcha->shouldBeInteger();
+        $captcha->shouldBe(3);
+    }
+
+    public function it_calculates_captcha_to_be_4_for_input_1111()
+    {
+        $result = $this->processInput('1111');
+
+        $result->shouldBe(4);
+    }
+
+    public function it_calculates_captcha_to_be_0_for_input_12345()
+    {
+        $result = $this->processInput('12345');
+
+        $result->shouldBe(0);
+    }
+
+    public function it_calculates_captcha_to_be_9_for_input_91212129()
+    {
+        $result = $this->processInput('91212129');
+
+        $result->shouldBe(9);
+    }
 }
--- src/Day1/Puzzle1.php
+++ src/Day1/Puzzle1.php
@@ -11,7 +11,49 @@ class Puzzle1 implements PuzzleInterface
         // no options to manage
     }
 
-    public function processInput(string $input)
+    public function processInput(string $input): int
     {
+        $data = $this->parseString($input);
+
+        $result = $this->calculateCaptcha($data);
+
+        return $result;
+    }
+
+    public function parseString(string $input): array
+    {
+        $input = trim($input);
+        $data = str_split($input, 1);
+
+        return $data;
+    }
+
+    public function calculateCaptcha(array $data): int
+    {
+        $dataLength = count($data);
+
+        $captcha = 0;
+
+        for ($i = 0; $i < $dataLength; $i++) {
+            $nextInSequence = $this->getNextInSequence($data, $i);
+
+            if ($data[$i] == $nextInSequence) {
+                $captcha += $data[$i];
+            }
+        }
+
+        return $captcha;
+    }
+
+    public function getNextInSequence(array $data, int $index): int
+    {
+        // At the end of the array? Loop back to the beginning
+        if ($index < count($data) - 1) {
+            $nextInSequence = $data[$index + 1];
+        } else {
+            $nextInSequence = $data[0];
+        }
+
+        return $nextInSequence;
     }
 }
